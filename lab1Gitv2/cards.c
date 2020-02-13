0508e854b74209391d67ec715e40ce138bc56734
beingPurple <ppg0291@gmail.com>
2020-02-12 22:09:45 -0500

    card update
    must commit to pull

--- lab1Gitv2/cards.c
+++ lab1Gitv2/cards.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
-card makeCard(char name[2],int val)
+card makeCard(char name[2], int val)
 {
     card v;
     strcpy(v.name, name[2]);
@@ -16,14 +16,54 @@ card makeCard(char name[2],int val)
 
     return v;
 
-
-
 }
- card genDeck(){
+card genDeck()
+{
     card newDeck[52];
 
-    newDeck[0] = makeCard("A♥",1);
-    newDeck[1] = makeCard("2♥",2);
+    newDeck[0] = makeCard("A♥", 1);
+    newDeck[1] = makeCard("2♥", 2);
+    newDeck[2] = makeCard("3♥", 3);
+    newDeck[3] = makeCard("4♥", 4);
+
     return newDeck[52];
+}
+
+card genh(char suit)
+{
+    int i;
+    std::string;
+    for (i = 0; i < 13; i++)
+    {
+        if (i == 0)
+        {
+            in =
+            {   "A", suit};
+            makeCard(in, i + 1);
+        }
+        else if (i < 0 && i < 10)
+        {
+        makeCard(
+                {   (i + 1) , suit}, i + 1);
+    }
+    else
+    {
+        if (i == 10)
+        {
+        makeCard(
+                {   "K" , suit}, 10);
+    }
+    else if (i == 11)
+    {
+    makeCard(
+            {   "Q" , suit}, 10);
+}
+else if (i == 12)
+{
+makeCard(
+        {   "J" , suit}, 10);
+}
+}
+}
 }
 
