01796a88ceb0f5e744acefbda491c50f5cb2cae3
beingPurple <ppg0291@gmail.com>
2020-02-15 14:41:47 -0500

    empty lab template
    will make it easier to make new labs

---
+++ EMPTY_LAB/main.c
@@ -0,0 +1,60 @@
+/************** ECE2049 DEMO CODE ******************/
+/**************  13 March 2019   ******************/
+/***************************************************/
+
+#include <msp430.h>
+
+/* Peripherals.c and .h are where the functions that implement
+ * the LEDs and keypad, etc are. It is often useful to organize
+ * your code by putting like functions together in files.
+ * You include the header associated with that file(s)
+ * into the main file of your project. */
+#include "peripherals.h"
+
+// Function Prototypes
+void swDelay(char numLoops);
+
+// Declare globals here
+
+// Main
+void main(void)
+
+{
+
+
+    WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer. Always need to stop this!!
+                                 // You can then configure it properly, if desired
+
+    initLeds();
+
+    configDisplay();
+    configKeypad();
+
+    // *** Intro Screen ***
+    Graphics_clearDisplay(&g_sContext); // Clear the display
+    // WRITE CODE BELOW
+
+
+}
+
+
+void swDelay(char numLoops)
+{
+	// This function is a software delay. It performs
+	// useless loops to waste a bit of time
+	//
+	// Input: numLoops = number of delay loops to execute
+	// Output: none
+	//
+	// smj, ECE2049, 25 Aug 2013
+
+	volatile unsigned int i,j;	// volatile to prevent removal in optimization
+			                    // by compiler. Functionally this is useless code
+
+	for (j=0; j<numLoops; j++)
+    {
+    	i = 50000 ;					// SW Delay
+   	    while (i > 0)				// could also have used while (i)
+	       i--;
+    }
+}
